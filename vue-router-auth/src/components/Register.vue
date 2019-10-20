d2e89abbc0e28eac4fe567f2117155658f681290
jtc7537 <jtc7537@gmail.com>
2019-10-19 21:07:01 -0400

    server

---
+++ vue-router-auth/src/components/Register.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h4>Register</h4>
+        <form>
+            <label for="name">Name</label>
+            <div>
+                <input id="name" type="text" v-model="name" required autofocus>
+            </div>
+
+            <label for="email">E-Mail Address</label>
+            <div>
+                <input id="email" type="email" v-model="email" required>
+            </div>
+
+            <label for="password">Password</label>
+            <div>
+                <input id="password" type="password" v-model="password" required>
+            </div>
+
+            <label for="password-confirmation" type="password" v-model="password_confirmation" required>Is this an administrator account?</label>
+            <div>
+                <select v-model="is_admin">
+                    <option value=1>Yes</option>
+                    <option value=2>No</option>
+                </select>
+            </div>
+
+            <div>
+                <button type="submit" @click="handleSubmit">
+                    Register
+                </button>
+            </div>
+        </form>
+    </div>
+</template>
